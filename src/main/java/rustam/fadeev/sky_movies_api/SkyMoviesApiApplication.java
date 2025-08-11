693c523dc383cbdee6d3686a1035427ca53becc0
Рустам Фадеев <trust.cod28@gmail.com>
2025-08-11 21:43:29 +0200

    init

---
+++ src/main/java/rustam/fadeev/sky_movies_api/SkyMoviesApiApplication.java
@@ -0,0 +1,12 @@
+package rustam.fadeev.sky_movies_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyMoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyMoviesApiApplication.class, args);
+	}
+}
---
+++ src/test/java/rustam/fadeev/sky_movies_api/SkyMoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rustam.fadeev.sky_movies_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyMoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
